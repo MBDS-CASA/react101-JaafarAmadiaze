8feb8b928535e4a1805f9ba328cebe2c7a3fa67f
Jaafar Amadiaze <Jaafar.Amadiaze@emsi-edu.ma>
2025-12-22 15:39:12 +0100

    TD01 Ajouter un composant Header dans la page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logoVert from "./assets/logo-vert.png";
 import './App.css'
 
+function Header() {
+return (
+  <div>
+     
+            <img src={logoVert} alt="Logo" width={200} />
+
+       
+         <h1>Introduction à React</h1>
+      <h4>A la découverte des premières notions de React</h4>
+      </div>
+)
+}
+
+function MainContent() {
+  return (
+    <main className="read-the-docs">
+      <p>Ici, nous afficherons des informations interessantes :) </p>
+    </main>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Jaafar + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+     
+      <MainContent />
+      
     </>
   )
 }
